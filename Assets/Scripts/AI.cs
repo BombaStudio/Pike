5313a2b1ef4165ed1863d44efc9da6235867f9b8
islamfazliyev <124509931+islamfazliyev@users.noreply.github.com>
2023-04-08 03:01:38 +0300

    ai basic v2

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
+    public bool isAlive = true;
     public float health;
     public Transform target;
     public GameObject sword;
     public float minimumDistance;
     public float maximumDistance;
     public float EnemySpeed;
+    public float EnemyJumpForce;
     public float attackCooldown;
     public float attackDuration;
     float lastAttack;
@@ -26,20 +28,33 @@ public class AI : MonoBehaviour
 
         if (health <= 0)
         {
-            rb.velocity = Vector2.zero;
+            isAlive = false;
         }
-        if (Vector2.Distance(transform.position, target.position) > maximumDistance)
+
+        
+        if (isAlive == true)
         {
-            rb.velocity = Vector2.zero;
-        }
-        else if (Vector2.Distance(transform.position, target.position) > minimumDistance)
-        {
-            EnemySpeed = 5;
-            transform.position = Vector2.MoveTowards(transform.position, target.position, EnemySpeed * Time.deltaTime);
-        }
-        else
-        {
-            attack();
+            if (Vector2.Distance(transform.position, target.position) > maximumDistance)
+            {
+                rb.velocity = Vector2.zero;
+            }
+            else if (Vector2.Distance(transform.position, target.position) > minimumDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, EnemySpeed * Time.deltaTime);
+                if (transform.position.x < target.position.x)
+                {
+                    transform.localScale = new Vector2(-1,1);
+                }
+
+                if (transform.position.x > target.position.x)
+                {
+                    transform.localScale = new Vector2(1,1);
+                }
+            }
+            else
+            {
+                attack();
+            }
         }
     }
 
